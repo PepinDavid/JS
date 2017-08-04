79d74a651d7d61b2718ba6684c5c28d4983afed5
David <david@live.fr>
2017-08-04 13:30:02 +0200

    ajout fonction addition, soustraction, produit avec un reel

--- Vrac/matrice.js
+++ Vrac/matrice.js
@@ -32,9 +32,8 @@ function produisMatrice (matrice1, matrice2, nbRecursive){
                 //creation 'var produit' pour additionner les produits de chaque ligne/colonne
                 var produit = 0;
                 for(var j = 0; j < matrice2.length; j++){
-                    console.log(arrLine[j] +" * "+matrice2[j][nbColumn]+' = '+arrLine[j] * matrice2[j][nbColumn])
+                    console.log(produit+" + "+arrLine[j] +" * "+matrice2[j][nbColumn]+' = '+ produit + arrLine[j] * matrice2[j][nbColumn])
                     produit += (arrLine[j] * matrice2[j][nbColumn]);
-                    console.log(produit)
                 }
                 //on place l'addition des produits lignes/colonnes dans la nouvelle matrice
                 newMatrice[i][nbColumn] = produit;
@@ -43,13 +42,97 @@ function produisMatrice (matrice1, matrice2, nbRecursive){
         }
         //si on a eu une recursivité c'est que l'on a éssaye la commutation matricielle
         if(nbRecursive < 1)
-            return {commutation: false, matrice: newMatrice};
+            return {
+                commutation: false,
+                matrice: newMatrice,
+                matrice1: matrice1,
+                matrice2: matrice2
+            };
         else
-            return {commutation: true, matrice: newMatrice};
+            return {
+                commutation: true,
+                matrice: newMatrice,
+                matrice1: matrice1,
+                matrice2: matrice2
+            };
     }else if(matrice2[0].length != matrice1.length && nbRecursive < 2){
         nbRecursive++;
         return produisMatrice(matrice2, matrice1, nbRecursive);
     }
       else
-        return {commutation: true, matrice: newMatrice};
+        return {
+            commutation: true,
+            matrice: newMatrice,
+            matrice1: matrice1,
+            matrice2: matrice2
+        };
+}
+
+function additionMatrice(matrice1, matrice2){
+    var newMatrice = [];
+    var lenLines1 = matrice1[0].length,
+    lenColumns1 = matrice1.length,
+    lenLines2 = matrice2[0].length,
+    lenColumns2 = matrice2.length;
+
+    if(lenLines1 == lenLines2 && lenColumns1 == lenColumns2){
+        newMatrice = new Array(lenLines1);
+
+        for(var i = 0; i < lenLines1; i++){
+            newMatrice[i] = new Array(lenColumns1);
+            for(var j = 0; j < lenColumns1; j++){
+                newMatrice[i][j] = matrice1[i][j] + matrice2[i][j];
+            }
+        }
+        return {
+            matrice: newMatrice,
+            matrice1: matrice1,
+            matrice2: matrice2
+        };
+    }
+
+    return {
+        matrice: newMatrice,
+        matrice1: matrice1,
+        matrice2: matrice2
+    };
+}
+
+function soustractionMatrice(matrice1, matrice2){
+    var newMatrice = [];
+    var lenLines1 = matrice1[0].length,
+    lenColumns1 = matrice1.length,
+    lenLines2 = matrice2[0].length,
+    lenColumns2 = matrice2.length;
+
+    if(lenLines1 == lenLines2 && lenColumns1 == lenColumns2){
+        newMatrice = new Array(lenLines1);
+
+        for(var i = 0; i < lenLines1; i++){
+            newMatrice[i] = new Array(lenColumns1);
+            for(var j = 0; j < lenColumns1; j++){
+                newMatrice[i][j] = matrice1[i][j] - matrice2[i][j];
+            }
+        }
+        return {matrice: newMatrice};
+    }
+
+    return {matrice: newMatrice};
+}
+
+function produitReelMatrice(nb, matrice){
+    var newMatrice = new Array(matrice.length);
+
+    for(var i = 0; i < matrice.length; i++){
+        var len = matrice[i].length;
+        newMatrice = new Array(len);
+        for(var j = 0; j < len; j++)
+            newMatrice[i][j] = nb * matrice[i][j];
+    }
+
+    return {
+        matrice: newMatrice,
+        reel: nb,
+        matrice1: matrice
+    };
 }
